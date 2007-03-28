a3ac87678fdea6f73a5b671a62d86154d250ffc1
Nur Monson <samiamwork@gmail.com>
2007-03-28 22:42:11 +0000

    - Added vertical lines to the gradient view and adjusted them some in the nib.

--- Source/CustomViews/TriviaGradientView.m
+++ Source/CustomViews/TriviaGradientView.m
@@ -26,9 +26,18 @@
 	TIPMutableGradientRef bgGradient = TIPMutableGradientCreate();
 	TIPGradientAddRGBColorStop(bgGradient,0.0f,0.8f,0.8f,0.8f,1.0f);
 	TIPGradientAddRGBColorStop(bgGradient,0.1f,0.9f,0.9f,0.9f,1.0f);
-	
 	TIPGradientAxialFillRect(cxt,bgGradient,*(CGRect *)&bounds,90.0f);
 	TIPGradientRelease(bgGradient);
+	
+	CGContextSetRGBStrokeColor(cxt,0.7f,0.7f,0.7f,1.0f);
+	CGContextSetLineWidth(cxt,2.0f);
+	CGContextMoveToPoint(cxt,bounds.origin.x,bounds.origin.y);
+	CGContextAddLineToPoint(cxt,bounds.origin.x,bounds.origin.y+bounds.size.height);
+	CGContextStrokePath(cxt);
+	
+	CGContextMoveToPoint(cxt,bounds.origin.x+bounds.size.width,bounds.origin.y);
+	CGContextAddLineToPoint(cxt,bounds.origin.x+bounds.size.width,bounds.origin.y+bounds.size.height);
+	CGContextStrokePath(cxt);
 }
 
 @end
